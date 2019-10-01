d6d3081caa851336bcc3c8c0b39aaae983ffc05b
Rayane Karoline Alves de Jesus <rayanek@kyros.com.br>
2019-10-01 08:58:00 -0300

    Curso Guanabara

--- Aula12Ex/ex014/script.js
+++ Aula12Ex/ex014/script.js
@@ -3,16 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 13
+    var hora = 19
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >=0 && hora < 12) {
         //BOM DIA!
         img.src = 'fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
     } else if (hora >=12 && hora <= 18) {
         //BOA TARDE!
         img.src = 'fototarde.jpg'
+        document.body.style.background = '#b9846f'
     } else {
         //BOA NOITE!
         img.src = 'fotonoite.jpg'
+        document.body.style.background = '#515154'
     }
 }
