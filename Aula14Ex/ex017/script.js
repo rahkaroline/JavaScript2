e1c8dac56b9fa0cb04cdee91ea6143a3b61cfd13
Rayane Karoline Alves de Jesus <rayanek@kyros.com.br>
2019-10-04 14:40:44 -0300

    Curso Guanabara

--- Aula14Ex/ex017/script.js
+++ Aula14Ex/ex017/script.js
@@ -1,4 +1,19 @@
 function contar() {
     var num = document.getElementById('num')
-    var res = document.getElementById('res')
+    var tab = document.getElementById('seltab')
+    if(num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+         var n = Number(num.value)
+         var c = 1
+         tab.innerHTML = ''
+         while(c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} * ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+         }
+    }
+   
 }
